849413a5db09ec76c0bfc4d9eb6fef0bd47ce595
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-03-10 12:05:20 +0100

    cors

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const app = require("./app");
-
 const PORT = process.env.PORT || 5005;
 
-// const http = require("http");
-// const server = http.createServer(app);
+const http = require("http");
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(app, {
+const io = new Server(server, {
   cors: {
-    origin: process.env.ORIGIN || "http://localhost:3000",
+    origin: process.env.ORIGIN || "https://waiterhack.herokuapp.com",
     //"http://localhost:3000" local,
     methods: ["GET", "POST"],
   },
-});
+})
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
+
   socket.on('join_room', (data) => {
+
     socket.join(data)
+
     io.emit('join_room', data); // This will emit the event to all connected sockets
   })
+
 })
 
-// server.listen(PORT, () => {
-//   console.log(`Server Socket listening on port 80`);
-// });
+server.listen(80, () => {
+  console.log(`Server Socket listening on port 80`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
