3f747ebf7dcadca4252de35b36b452eff63910a0
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-03-10 18:07:37 +0100

    sozket heroku docs

--- server.js
+++ server.js
@@ -1,44 +1,53 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5005;
 
-const http = require("http");
-const socketServer = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(socketServer, {
-  cors: {
-    origin: process.env.ORIGIN || "https://waiterhack.herokuapp.com",
-    //"http://localhost:3000" local,
-    methods: ["GET", "POST"],
-  },
+const server = app.listen(PORT, () => {
+  console.log("Listening on port: " + PORT);
 })
+
+const io = require('socket.io')(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on('join_room', (data) => {
+    socket.join(data)
+    io.emit('join_room', data)
+  })
+})
+
+
+
+// ---------
+
+
+
+// const app = require("./app");
+// const PORT = process.env.PORT || 5005;
+
+// const http = require("http");
+// const socketServer = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(socketServer, {
+//   cors: {
+//     origin: process.env.ORIGIN || "https://waiterhack.herokuapp.com",
+//     //"http://localhost:3000" local,
+//     methods: ["GET", "POST"],
+//   },
+// })
 
 // io.on("connection", (socket) => {
 //   console.log("a user connected", socket.id);
 
-<<<<<<< HEAD
-  socket.on('join_room', (data) => {
-    socket.join(data)
-    io.emit('join_room',  data); // This will emit the event to all connected sockets
-  })
-
-})
-
-
-// Server.listen(3001, () => {
-//   console.log(`Server Socket listening on port 80`);
-// });
-=======
 //   socket.on('join_room', (data) => {
 //     socket.join(data)
-//     io.emit('join_room',  data); // This will emit the event to all connected sockets
+//     io.emit('join_room', data); // This will emit the event to all connected sockets
 //   })
 
 // })
->>>>>>> 3945827416239605fe0ff7a81dd5bc1fbf6548f8
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// })
 
 // socketServer.listen(PORT, () => {
 //   console.log(`Sockets on port ${PORT}`);
