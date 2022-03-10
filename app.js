85394a50355dfcf2e9e992ad74083f703d1600b0
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-03-10 12:21:23 +0100

    cors

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(function (req, res, next) {
+    let origin = req.headers.origin;
+    res.header("Access-Control-Allow-Origin", req.headers.host.indexOf("localhost") > -1 ? "http://localhost:3000" : origin);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+})
+
 require("./config")(app);
 
 const allRoutes = require("./routes/index.routes");
