8c0ee710edd7c83797ea37d95df1e56a40b1563e
Gonzalo Otero <gonzalo.chalo@gmail.com>
2022-03-10 19:16:04 +0100

    merge

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ const io = new Server(socketServer, {
   },
 })
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected", socket.id);
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
 
-<<<<<<< HEAD
   socket.on('join_room', (data) => {
     socket.join(data)
-    io.emit('join_room',  data); // This will emit the event to all connected sockets
+    io.emit('join_room', data); // This will emit the event to all connected sockets
   })
 
 })
@@ -27,14 +26,6 @@ const io = new Server(socketServer, {
 // Server.listen(3001, () => {
 //   console.log(`Server Socket listening on port 80`);
 // });
-=======
-//   socket.on('join_room', (data) => {
-//     socket.join(data)
-//     io.emit('join_room',  data); // This will emit the event to all connected sockets
-//   })
-
-// })
->>>>>>> 3945827416239605fe0ff7a81dd5bc1fbf6548f8
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
@@ -42,4 +33,4 @@ app.listen(PORT, () => {
 
 // socketServer.listen(PORT, () => {
 //   console.log(`Sockets on port ${PORT}`);
-// })
+// }
